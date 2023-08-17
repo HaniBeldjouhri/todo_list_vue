d69b014bb7205c529b556b3af76b5eedc728c5d9
Hani <hani.beldjouhri@gmail.com>
2023-08-17 09:53:02 +0200

    create new files

---
+++ src/components/Task.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="task">
+    {{ task.text }}
+    <button @click="deleteTask" class="Delete">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+  methods: {
+    deleteTask() {
+      this.$emit("deleteTask", this.task.id);
+    },
+  },
+};
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tasks">
+    <Task
+      v-for="task in tasks"
+      :key="task.id"
+      :task="task"
+      @deleteTask="deleteTask"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  name: "Tasks",
+  components: {
+    Task,
+  },
+  props: {
+    tasks: Array,
+  },
+  methods: {
+    deleteTask(taskId) {
+      let updatedTasks = this.tasks.filter((task) => task.id !== taskId);
+      this.$emit("deleteTask", taskId, updatedTasks); 
+    },
+  },
+};
+</script>
